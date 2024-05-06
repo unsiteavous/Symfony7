78f9400246f4bcf0e30feffbade151d0e09664f8
Théophile Bellintani <theophile@captp.fr>
2024-05-06 15:02:41 +0200

    fix: correction de boucle for i et j

--- symfo7DWWM3/src/DataFixtures/FilmFixtures.php
+++ symfo7DWWM3/src/DataFixtures/FilmFixtures.php
@@ -10,43 +10,44 @@ use Faker\Factory;
 
 class FilmFixtures extends Fixture implements DependentFixtureInterface
 {
-    private $faker;
-    public function __construct()
-    {
-        $this->faker = Factory::create('fr_FR');
-    }
-    public function load(ObjectManager $manager): void
-    {
-        for ($i = 0; $i < 10; $i++) {
-            $film = new Film;
-            $film
-              ->setNom($this->faker->words(2, true))
-              ->setDuree($this->faker->dateTimeBetween('-9 hour', '-5 hours'))
-              ->setDateSortie($this->faker->dateTimeBetween('28/18/1895', '+1 year'))
-              ->setUrlAffiche($this->faker->url())
-              ->setLienTrailer($this->faker->url())
-              ->setResume($this->faker->text(300))
-              ->setClassification($this->getReference($this->faker->randomElement(ClassificationFixtures::getClassificationArray())))
-              ;
+  private $faker;
+  public function __construct()
+  {
+    $this->faker = Factory::create('fr_FR');
+  }
+  public function load(ObjectManager $manager): void
+  {
+    for ($i = 0; $i < 10; $i++) {
+      $film = new Film;
+      $film
+        ->setNom($this->faker->words(2, true))
+        ->setDuree($this->faker->dateTimeBetween('-9 hour', '-5 hours'))
+        ->setDateSortie($this->faker->dateTimeBetween('28/18/1895', '+1 year'))
+        ->setUrlAffiche($this->faker->url())
+        ->setLienTrailer($this->faker->url())
+        ->setResume($this->faker->text(300))
+        ->setClassification($this->getReference($this->faker->randomElement(ClassificationFixtures::getClassificationArray())));
 
-              for ($i=0; $i < $this->faker->numberBetween(0, 5); $i++) {
-                $categoriesUtilisees = [];
-                foreach($film->getCategories() as $categorie){
-                  $categoriesUtilisees[] = $categorie->getNom();
-                } 
-                $categoriesNonUtilisees = array_diff( CategorieFixtures::getCategorieArray(),$categoriesUtilisees);
-                
-                $film->addCategory($this->getReference($this->faker->randomElement($categoriesNonUtilisees)));
-              }
-
-            $manager->persist($film);
+      $limite = $this->faker->numberBetween(0, 5);
+      for ($j = 0; $j < $limite; $j++) {
+        $categoriesUtilisees = [];
+        foreach ($film->getCategories() as $categorie) {
+          $categoriesUtilisees[] = $categorie->getNom();
         }
+        $categoriesNonUtilisees = array_diff(CategorieFixtures::getCategorieArray(), $categoriesUtilisees);
 
-        $manager->flush();
+        $categorieReference = $this->faker->randomElement($categoriesNonUtilisees);
+        $film->addCategory($this->getReference($categorieReference));
+      }
+
+      $manager->persist($film);
     }
 
-    public function getDependencies() 
-    {
-      return [ClassificationFixtures::class, CategorieFixtures::class];
-    }
+    $manager->flush();
+  }
+
+  public function getDependencies()
+  {
+    return [ClassificationFixtures::class, CategorieFixtures::class];
+  }
 }
