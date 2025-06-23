994463ff638fd99a9ca57866de89fb6b64015014
Théophile Bellintani <theophile@captp.fr>
2025-06-23 16:26:41 +0200

    fixtures

--- coursCDA/src/DataFixtures/FilmFixtures.php
+++ coursCDA/src/DataFixtures/FilmFixtures.php
@@ -12,19 +12,24 @@ class FilmFixtures extends AbstractFixtures
 {
     public function load(ObjectManager $manager): void
     {
-        $film = (new Film)
-            ->setName($this->faker->words(3, true))
-            ->setDuration(DateTimeImmutable::createFromFormat("H:i:s", "1:56:30"))
-            ->setUrlAffiche($this->faker->imageUrl())
-            ->setUrlTrailer($this->faker->url())
-            ->setResume($this->faker->text())
-            ->setDateSortie(DateTimeImmutable::createFromFormat("Y-m-d", "2022-01-01"))
-            ->setClassification($this->getReference(ClassificationFixtures::getClassifications()[$this->faker->numberBetween(0, 3)], Classification::class))
-            ->addCategory($this->getReference(CategoryFixtures::getCategories()[$this->faker->numberBetween(0, 7)], Category::class));
+        for ($i = 0; $i < 10; $i++) {
+            $film = (new Film)
+                ->setName($this->faker->words(3, true))
+                ->setDuration(DateTimeImmutable::createFromFormat("H:i:s", "1:56:30"))
+                ->setUrlAffiche($this->faker->imageUrl())
+                ->setUrlTrailer($this->faker->url())
+                ->setResume($this->faker->text())
+                ->setDateSortie(DateTimeImmutable::createFromFormat("Y-m-d", "2022-01-01"))
+                ->setClassification($this->getReference(ClassificationFixtures::getClassifications()[$this->faker->numberBetween(0, 3)], Classification::class));
 
-        $manager->persist($film);
+            for ($j = 0; $j < $this->faker->numberBetween(0, 5); $j++) {
+                $film->addCategory($this->getReference(CategoryFixtures::getCategories()[$this->faker->numberBetween(0, 7)], Category::class));
+            }
+
+            $manager->persist($film);
+        }
+
         $manager->flush();
-
     }
 
     public function getDependencies()
